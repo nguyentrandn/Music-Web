8029d7d7465f5b7aabd34140fb6eb79e5ca12888
unknown <longtma123@gmail.com>
2022-04-21 19:02:41 +0700

    choose Song and reponseve

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const progress = $$("#range")
 const next = $$(".btn-next")
 const prev = $$(".btn-prev")
 const time = $$("#time")
+const playList = $$(".menu-list")
 
 const app = {
     currentIndex: 0,
@@ -47,16 +48,16 @@ const app = {
         },
     ],
     render: function () {
-        const htmls = this.songs.map( song => {
+        const htmls = this.songs.map( (song, index) => {
             return `
-                <li class="song-item">
-                    <img src="${song.img}" alt="" id="img" class="col col-lg-1">
+                <li class="song-item ${index === this.currentIndex ? 'song-active' : ""}" data-index="${index}"> 
+                    <img src="${song.img}" alt="" id="img" class="col col-1">
                     <label for="img"  class="col col-lg-8">${song.name}</label>
-                    <div class="decription col-lg-2"><ion-icon name="more"></ion-icon></div>
+                    <div class="decription col-2"><ion-icon name="more"></ion-icon></div>
                 </li>
             `
         })
-        $$(".menu-list").innerHTML = htmls.join("")
+        playList.innerHTML = htmls.join("")
     },
     defineProperties: function () {
         Object.defineProperty(this, 'curentSong',{
@@ -80,6 +81,16 @@ const app = {
                 audio.play()
             }  
         }
+        // Xử lí khi click vào dấu Space
+        $(document).on('keypress', function(e) {
+            if (e.which == 32) {
+                if (app.isPlay) {
+                    audio.pause()
+                } else {
+                    audio.play()
+                }  
+            }
+        })
         // Khi bai Hat dc Play
         audio.onplay = function(){
             app.isPlay = true
@@ -96,7 +107,7 @@ const app = {
                 // làm tròn theo %
                 const progressPercent = Math.floor(audio.currentTime / audio.duration * 100)
                 progress.value = progressPercent
-                console.log(progressPercent);
+                
                 time.innerHTML = Math.floor(audio.duration / 60) + ":00"
             }
         }
@@ -110,17 +121,41 @@ const app = {
         next.onclick = function(){
             app.nextSong()
             audio.play()
+            app.render() // render lại để Active song trong List
         }
         //Khi Prev Song
         prev.onclick = function () {
             app.prevSong()
             audio.play()
+            app.render() // render lại để Active song trong List
+
         }
         //Khi end song 
         audio.onended = function () {
             next.onclick()
         }
+        // Click vào List Song
+        playList.onclick = function (e) {
+            const songNode = e.target.closest('.song-item:not(.song-active)')
+          if( songNode|| e.target.closest('.decription ')){
+             
+              // Xử lí khi click vào Song
+              if (songNode) {
+                  console.log(songNode.getAttribute('data-index'));
+                  app.currentIndex = Number(songNode.getAttribute('data-index'))
+                  app.loadcurrenSong()
+                  app.render()
+                  audio.play()
+              }
+              //Xử lí khi click vào dấu "..."
+              if (e.target.closest('.decription ')) {
+                  
+              }
+          };
+        }
+
     },
+    
     nextSong: function () {
         this.currentIndex ++
         if (this.currentIndex >= this.songs.length) {
